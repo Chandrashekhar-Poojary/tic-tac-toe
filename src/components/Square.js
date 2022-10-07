943bd89b980b6469a0051cc3614bc11715c11261
chandra <poojarychandra1309@gmail.com>
2022-10-07 16:09:09 +0530

    created components

---
+++ src/components/Square.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+//const Square = props => { // before destructuring 
+const Square = ({value}) =>{ // after object destructuring ({})
+    //console.log(props);
+    return <button type="button">{ value }</button> // instead of props.value just define value
+};
+
+export default Square;
