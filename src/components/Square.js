0ab601f8e554f2e55e8566d21bd2ae807a084caf
chandra <poojarychandra1309@gmail.com>
2022-10-07 23:47:49 +0530

    git init

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 //const Square = props => { // before destructuring 
-const Square = ({value}) =>{ // after object destructuring ({})
+const Square = ({value, onClick}) =>{ // after object destructuring ({})
     //console.log(props);
-    return <button type="button" className='square'>{ value }</button> // instead of props.value just define value
+    return (
+        <button type="button" className='square' 
+            onClick={onClick} // onClick is a custom prop
+        >
+            { value }
+        </button> // instead of props.value just define value
+    );
 };
 
 export default Square;
