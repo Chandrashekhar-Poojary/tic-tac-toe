23b57140750329e1da4fa8fafd84c2e1e24d9e3b
chandra <poojarychandra1309@gmail.com>
2022-10-08 11:37:35 +0530

    init commited

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import Board from "./components/Board";
 import History from "./components/History";
 import { calculateWinner } from "./helpers";
+import StatusMessage from "./components/StatusMessage";
 import "./styles/root.scss";
 
 const App = () =>{
@@ -18,9 +19,7 @@ const App = () =>{
  
   const winner = calculateWinner(current.board);
   //console.log(winner);
-  const message = winner 
-  ? `Winner is ${winner}`
-  : `Next player is ${current.isXNext ? 'X':'O'}`;
+  
 
  const handleSquareClick = (position) =>{
 
@@ -48,7 +47,7 @@ const App = () =>{
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <h2>{message}</h2>
+      <StatusMessage winner={winner} current={current}/>
       <Board board={current.board} handleSquareClick={handleSquareClick}/>
       <History history={history} moveTo={moveTo} currentMove={currentMove}/>
     </div>
