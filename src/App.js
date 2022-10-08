1b086010f27d5d13ab1486891a47252bdf704f5b
chandra <poojarychandra1309@gmail.com>
2022-10-08 11:19:00 +0530

    committed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import Board from "./components/Board";
+import History from "./components/History";
 import { calculateWinner } from "./helpers";
 import "./styles/root.scss";
 
@@ -17,7 +18,6 @@ const App = () =>{
  
   const winner = calculateWinner(current.board);
   //console.log(winner);
-
   const message = winner 
   ? `Winner is ${winner}`
   : `Next player is ${current.isXNext ? 'X':'O'}`;
@@ -27,7 +27,6 @@ const App = () =>{
     if(current.board[position] || winner){ // cannot rewrite on board
       return;
     }
-
     setHistory( (prev) =>{
       const last = prev[ prev.length - 1 ];
       const newBoard = last.board.map((square, pos) =>{
@@ -39,15 +38,19 @@ const App = () =>{
       return prev.concat({board: newBoard, isXNext: !last.isXNext})
     });
     // setIsXNext((prev)=> !prev);
-
     setCurrentMove(prev => prev + 1);
  };
+
+ const moveTo = move =>{
+  setCurrentMove(move);
+ }
 
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
       <h2>{message}</h2>
       <Board board={current.board} handleSquareClick={handleSquareClick}/>
+      <History history={history} moveTo={moveTo} currentMove={currentMove}/>
     </div>
   );
 }
